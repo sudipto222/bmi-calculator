d42c549a7828467adfb36f424102b76bb44cea19
sudipto222 <sudiptomukh@gmail.com>
2021-07-03 17:06:24 +0600

    update

--- test/bmi.test.js
+++ test/bmi.test.js
@@ -5,28 +5,36 @@ const getByCategory = require('../index.js');
 
 describe('Get Overweight person in table', () => {
   it('should return total length 4', () => {
-    const result = getByCategory('Overweight');
-    assert.equal(result, 4);
+    setTimeout(() => {
+      const result = getByCategory('Overweight');
+      assert.equal(result, 4);
+    }, 1000)
   });
 });
 
 describe('Get Moderately obese person in table', () => {
   it('should return total length 12', () => {
-    const result = getByCategory('Moderately obese');
-    assert.equal(result, 12);
+    setTimeout(() => {
+      const result = getByCategory('Moderately obese');
+      assert.equal(result, 12);
+    }, 1000)
   });
 });
 
 describe('Get Normal weight person in table', () => {
   it('should return total length 10', () => {
-    const result = getByCategory('Normal weight');
-    assert.equal(result, 10);
+    setTimeout(() => {
+      const result = getByCategory('Normal weight');
+      assert.equal(result, 10);
+    }, 1000)
   });
 });
 
 describe('Get Severely obese person in table', () => {
   it('should return total length 0', () => {
-    const result = getByCategory('Severely obese');
-    assert.equal(result, 0);
+    setTimeout(() => {
+      const result = getByCategory('Severely obese');
+      assert.equal(result, 0);
+    }, 1000);
   });
 });
